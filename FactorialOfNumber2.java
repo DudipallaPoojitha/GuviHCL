fe7eae1a6b0c03451f46aa1e0d41dbd98847032b
DudipallaPoojitha <dudipallapoojitha@gmail.com>
2026-01-19 16:45:20 +0530

    Add files via upload

---
+++ FactorialOfNumber2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FactorialOfNumber2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int result=1;
+        if(N==0){
+            System.out.println("1");
+        }
+        else{
+            for(int i=1;i<=N;i++){
+                result=result*i;
+            }
+            System.out.println(result);
+        }
+        
+        
+    }
+    
+}
